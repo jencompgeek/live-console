097bf1856b47661dc1a76c6dbefdebec8b303670
Jennifer Hickey <jhickey@vmware.com>
2012-01-27 11:22:19 -0800

    Simplify build
    - Build through "gem build" and install using "gem install"

--- lib/live_console_config.rb
+++
@@ -1,8 +0,0 @@
-# This module houses a pile of informative constants for LiveConsole.
-module LiveConsoleConfig
-  Authors = 'Pete Elmore'
-  Email = 'pete.elmore@gmail.com'
-  PkgName = 'live_console'
-  Version = '0.2.4'
-  Project = 'live-console'
-end
